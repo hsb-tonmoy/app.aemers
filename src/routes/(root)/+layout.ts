73d33ec7022d69f15fbf7cb21fc00db3721b2f12
hsb.tonmoy <hsb.tonmoy@gmail.com>
2022-09-13 16:19:00 -0400

    fixed server layout issue

--- src/routes/(root)/+layout.ts
+++ src/routes/(root)/+layout.ts
@@ -2,19 +2,18 @@ import * as api from '$lib/api';
 import { application_status, application_steps } from '$lib/data/stores';
 import { error, redirect } from '@sveltejs/kit';
 
-export async function load({ locals }) {
-	if (!locals.user) {
+export async function load({ parent }) {
+	const { user, access } = await parent();
+
+	if (!user) {
 		throw redirect(302, '/login');
 	}
 
-	if (locals.user && !locals.user.isEvaluated) {
+	if (user && !user.isEvaluated) {
 		throw redirect(302, '/evaluation');
 	}
 
-	const application_status_fetch = await api.get(
-		`application_status/${locals.user.pk}/`,
-		locals.access
-	);
+	const application_status_fetch = await api.get(`application_status/${user.pk}/`, access);
 
 	const application_status_data = await application_status_fetch.json();
 
