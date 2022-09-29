abef3683c1b765f1a192a773be29b32dcf0c7cb6
hsb.tonmoy <hsb.tonmoy@gmail.com>
2022-09-29 18:11:20 -0400

    added uppy

---
+++ src/routes/(root)/application/documents_upload/test/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Uppy from '@uppy/core';
+	import { DragDrop } from '@uppy/svelte';
+
+	const uppy = new Uppy();
+</script>
+
+<div class="flex flex-col px-14 py-8 xl:px-20 xl:py-12 bg-white rounded-2xl w-full">
+	<div class="flex gap-x-20 w-full">
+		<div class="w-2/4">
+			<p class="text-secondary leading-relaxed text-base">
+				Submit all of your authentic documents here. Our team will review and approve these
+				documents. These documents will be used in your admission process. We ensure the privacy of
+				your documents.
+			</p>
+		</div>
+		<div class="w-2/4">
+			<h6 class="text-primary font-bold text-xl mb-2">Instructions</h6>
+			<ol class="list-decimal list-inside flex flex-col gap-y-1 text-secondary text-sm">
+				<li>Don’t provide Black and White Document.</li>
+				<li>Scan and upload pdf document.</li>
+				<li>Provide a clear document.</li>
+				<li>Don’t provide an expired document.</li>
+			</ol>
+		</div>
+	</div>
+	<DragDrop {uppy} plugins={['Webcam']} />
+</div>
