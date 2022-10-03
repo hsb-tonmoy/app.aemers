44fa90259f6d0842e7cc62c216282249073284fa
hsb-tonmoy <hsb.tonmoy@gmail.com>
2022-10-03 01:53:43 -0400

    fixed application percentage

--- src/lib/data/stores.ts
+++ src/lib/data/stores.ts
@@ -3,7 +3,7 @@ import { derived, writable } from 'svelte/store';
 export const application_status = writable();
 export const application_percentage = derived(application_status, ($application_status) => {
 	const items = [
-		// $application_status.applicated_started,
+		$application_status.application_started,
 		$application_status.file_opening,
 		$application_status.pre_application_form,
 		$application_status.documents_upload,
@@ -16,7 +16,8 @@ export const application_percentage = derived(application_status, ($application_
 		$application_status.pre_departure_session,
 		$application_status.welcome_to_usa
 	];
-	const done = items.filter((item) => item === 2 || item === true).length;
+	const done = items.filter((item) => item === 2 || item == true).length;
+
 	const percentage = Math.round((((done + 1) / items.length) * 100) / 10) * 10;
 	return percentage;
 });
