c0044dee18abfa5cb854eb456401a7763a23b4ed
hsb-tonmoy <hsb.tonmoy@gmail.com>
2022-11-09 02:02:37 -0500

    trying wav encoder

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
-// import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 
 async function send({ method, path, data, token = '', file = false }) {
 	const opts = {
